c1418393e49d9824ef5623c73892d75a05325843
linhchipham <chiptlhe153641@fpt.edu.vn>
2021-11-03 12:32:57 +0700

    merge fix

--- src/java/Controller/Manager/MServiceControl.java
+++ src/java/Controller/Manager/MServiceControl.java
@@ -32,15 +32,8 @@ public class MServiceControl extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet MServiceControl</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet MServiceControl at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        
+            
         }
     }
 
