5d310fc241671bc8de271585d475e978d180ef33
linhchipham <chiptlhe153641@fpt.edu.vn>
2021-11-08 00:57:05 +0700

    add Mdetail

--- src/java/Controller/Filter/ManagerAuthenticationFilter.java
+++ src/java/Controller/Filter/ManagerAuthenticationFilter.java
@@ -30,7 +30,7 @@ public class ManagerAuthenticationFilter implements Filter {
     private static final String[] loginRequiredURLs = {
         "PostControler", "postDetail", "requestPostDetail", "updatePostDetail", "CustomerControl",
         "searchCustomerControl", "updateCustomerControl", "CustomerDetail", "AddCustomer",
-        "FeedbackDetailControl","FeedbackControl", "MServiceControl", "MServiceDetail","/MDetail.jsp"
+        "FeedbackDetailControl","FeedbackControl", "MServiceControl", "MServiceDetail","/MDetail.jsp","/MService"
     };
 
     @Override
